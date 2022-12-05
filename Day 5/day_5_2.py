7a8c13897483cd1998fd12d9f91ede7a23ade50a
Eko Widianto <ekowidianto@outlook.com>
2022-12-06 00:14:04 +0800

    AOC day 5

---
+++ Day 5/day_5_2.py
@@ -0,0 +1,41 @@
+
+def initial_position():
+    input = {'1': 'DLJRVGF',
+             '2': 'TPMBVHJS',
+             '3': 'VHMFDGPC',
+             '4': 'MDPNGQ',
+             '5': 'JLHNF',
+             '6': 'NFVQDGTZ',
+             '7': 'FDBL',
+             '8': 'MJBSVDN',
+             '9': 'GLD'}
+    for key, value in input.items():
+        input[key] = list(value)
+    return input
+
+
+def find_top_of_stacks():
+    lines = read_file('input.txt')
+    crates = initial_position()
+    top_crates = []
+    for line in lines:
+
+        line_split = line.split(' ')
+        crate_num, src, dst = int(line_split[1]), line_split[3], line_split[5]
+        crates_to_move, crates[src] = crates[src][-crate_num:], crates[src][:-crate_num]
+        crates[dst].extend(crates_to_move)
+
+    for _, value in crates.items():
+        top_crates.append(value[-1])
+    print(''.join(top_crates))
+    return top_crates
+
+
+def read_file(filename):
+    with open(filename) as file:
+        contents = file.read().splitlines()
+    return contents
+
+
+if __name__ == '__main__':
+    find_top_of_stacks()
